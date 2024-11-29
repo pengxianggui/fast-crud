552a7bbd084b9b2954cb9a75ac47cc14d0377656
pengxg <xiangguipeng@zstest.com>
2024-11-29 17:55:18 +0800

    add demo

---
+++ demo/src/main/java/io/github/pengxianggui/service/demo/TestService.java
@@ -0,0 +1,8 @@
+package io.github.pengxianggui.service.demo;
+
+import io.github.pengxianggui.crud.BaseService;
+import io.github.pengxianggui.domain.demo.Test;
+
+public interface TestService extends BaseService<Test> {
+
+}
