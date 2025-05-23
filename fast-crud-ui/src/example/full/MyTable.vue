1be1911da2545f7714ad520bd277a924b51c5b5d
pengxg <pengxianggui@outlook.com>
2025-05-23 21:55:38 +0800

    update

--- fast-crud-ui/src/example/full/MyTable.vue
+++ fast-crud-ui/src/example/full/MyTable.vue
@@ -239,7 +239,7 @@ export default {
       const {row, editRow, config, status} = fatRow
       this.$refs['fastTable'].updateForm(fatRow)
     },
-    handleAgeChange(age, {row, editRow, status, column, config, $index}) {
+    handleAgeChange(age, {row: {row, editRow, status, config}, column, $index}) {
       console.log('index:', $index);
       console.log('status:', status);
       console.log('editRow:', editRow);
